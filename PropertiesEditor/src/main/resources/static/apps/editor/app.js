121682b539b06a458050be97ae3cc85a5458a234
P758504 <christopher.chapman@nab.com.au>
2017-04-09 16:25:08 +1000

    Added a angular search

--- PropertiesEditor/src/main/resources/static/apps/editor/app.js
+++ PropertiesEditor/src/main/resources/static/apps/editor/app.js
@@ -10,6 +10,7 @@
 	app = angular.module('propertyEditor', deps);
 
 	app.controller('PropertyEditorController', function($scope, $http) {
+		var checkBranch;
 		$scope.tree_event_handler = function(branch) {
 			var _ref;
 			
@@ -22,6 +23,75 @@
 				});
 			}, $scope.items);
 
+		};
+
+		$scope.treeControl = {};
+		
+		
+		
+		
+		$scope.search={_text:"",
+				_match:0,
+				set text (name) {
+					this._text = name;
+					this._match = 0;
+				},
+				get text () {
+					return this._text;
+				},
+				_checkBranch:function(text,branch)
+				{
+					if (branch.value)
+					{
+						for(var key in branch.value){
+							var valueText = branch.value[key];
+							if(text != null && valueText != null 
+								    && typeof text === 'string' && typeof valueText === 'string'
+								        && text.toUpperCase() === valueText.toUpperCase())
+							{
+								return true;
+							}
+						}
+					}
+					return false;
+				},
+				_visitBranch:function(text,branch)
+				{
+					if(this._checkBranch(text,branch))
+						return branch;
+					var child = $scope.treeControl.get_first_child(branch);
+					while(child)
+					{
+						var ret = this._visitBranch(text,child);
+						if (ret)
+						{
+							return ret;
+						}
+						child = $scope.treeControl.get_next_sibling(child);
+					}
+					return null;
+				},
+				findNext:function() {
+					var child = $scope.treeControl.get_first_branch();
+					var branch;
+					for (var i = 0; i <= this._match && child;)
+					{
+						branch = this._visitBranch(this._text,child);
+						child = $scope.treeControl.get_next_sibling(child);
+
+						if(branch)
+						{
+							i++;
+						}
+					}
+					if(branch)
+					{
+						this._match++;
+						$scope.treeControl.collapse_all();
+						$scope.treeControl.select_branch(branch);
+					}
+					
+				}
 		};
 		$scope.data = [];
 		$http.get('editableProperties').then(function(response) {
